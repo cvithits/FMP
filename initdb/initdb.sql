f13c4cb3dd551024bc7c505178409f0da2d958be
cvithits <v_chayuth@hotmail.com>
2022-09-08 21:03:40 +0700

    added timestamp

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -1,31 +1,39 @@
 CREATE TABLE IF NOT EXISTS public.stock (
-	symbol text NULL PRIMARY KEY,
-    stockName text NULL
-);
-  
-CREATE TABLE IF NOT EXISTS public.delistedCompany (
-	id serial NOT NULL PRIMARY KEY,
-	symbolId text NOT NULL ,
-    companyName text NULL, 
-	exchange text NULL,
-	ipoDate date NULL,
-	delistedDate date NULL,
-  constraint fk_stock
-     foreign key (symbolId) 
-     REFERENCES stock (symbol)
+	symbol text NOT NULL,
+    stockname text NULL,
+    created_time timestamptz NULL DEFAULT NOW(),
+    updated_time timestamptz NULL DEFAULT NOW(),
+    CONSTRAINT stock_pkey PRIMARY KEY (symbol)
 );
 
-CREATE TABLE IF NOT EXISTS public.historicalDevidend (
+CREATE TABLE IF NOT EXISTS public.delistedcompany (
 	id serial NOT NULL PRIMARY KEY,
-	symbolId text NOT NULL,
-	dividendDate date NULL,
+	symbolid text NOT NULL ,
+    companyname text NULL, 
+	exchange text NULL,
+	ipodate date NULL,
+	delisteddate date NULL,
+    created_time timestamptz NULL DEFAULT NOW(),
+    updated_time timestamptz NULL DEFAULT NOW(),
+    CONSTRAINT stock_fk foreign key (symbolId) REFERENCES stock (symbol),
+    CONSTRAINT delistedcompany_uq UNIQUE (symbolId, companyname, exchange)
+);
+
+  
+  
+CREATE TABLE IF NOT EXISTS public.historicaldividend (
+	id serial NOT NULL PRIMARY KEY,
+	symbolid text NOT NULL,
+	dividenddate date NULL,
 	label text NULL,
-	adjDividend numeric(21,15)  NULL,
+	adjdividend numeric(21,15)  NULL,
 	dividend numeric(12,6) NULL,
-	recordDate date NULL,
-	paymentDate date NULL,
-	declarationDate date NULL,
-  constraint fk_stock
-     foreign key (symbolId) 
-     REFERENCES stock (symbol)
+	recorddate date NULL,
+	paymentdate date NULL,
+	declarationdate date NULL,
+    created_time timestamptz NULL DEFAULT NOW(),
+    updated_time timestamptz NULL DEFAULT NOW(),
+    CONSTRAINT stock_fk foreign key (symbolId) REFERENCES stock (symbol),
+    CONSTRAINT historicaldividend_uq UNIQUE (symbolId, dividenddate)
+    
 );
